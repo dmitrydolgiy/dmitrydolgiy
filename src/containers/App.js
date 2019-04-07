d6a793d25ac9cee3eaabd3968d58235a6910ba71
dmitrydolgiy <dmitry.dolgiy@gmail.com>
2019-04-08 01:55:39 +0300

    first commit

--- src/containers/App.js
+++
@@ -1,8 +0,0 @@
-import { connect } from 'react-redux';
-import App from '../components/App';
-import * as actionCreators from '../actions';
-
-const mapStateToProps = ({ currentTaskValue, activeStateButton, listOfTasks, }) =>
-  ({ currentTaskValue, activeStateButton, listOfTasks });
-
-export default connect(mapStateToProps, actionCreators)(App);
